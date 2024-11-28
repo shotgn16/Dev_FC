f8742a7600b000fcb4efe6e39a7f06bf64c8b478
Jack Barnard <jack.tm68@googlemail.com>
2024-11-28 22:05:33 +0000

    Cleaned up Landing page (Home)

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ForestChurches.Pages
 {
@@ -14,7 +13,6 @@ namespace ForestChurches.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
